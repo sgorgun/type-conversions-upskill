4111c5dbc82d7df9e4b6c54a077b6df60c500dc9
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2023-04-03 19:25:59 +0000

    Update BoxingConversions.cs

--- TypeConversions/BoxingConversions.cs
+++ TypeConversions/BoxingConversions.cs
@@ -13,39 +13,11 @@ namespace TypeConversions
         public static object BoxToObject(Point point) => throw new NotImplementedException();
 
         /// <summary>
-        /// Performs an boxing conversion custom <see cref="Point"/> struct to <see cref="ValueType"/> class.
-        /// </summary>
-        /// <param name="point"><see cref="Point"/> object.</param>
-        /// <returns><see cref="ValueType"/> object.</returns>
-        public static ValueType BoxToValueType(Point point) => throw new NotImplementedException();
-
-        /// <summary>
-        /// Performs an boxing conversion custom <see cref="Point"/> struct to <see cref="IColorable"/> interface.
-        /// </summary>
-        /// <param name="point"><see cref="Point"/> object.</param>
-        /// <returns><see cref="IColorable"/> object.</returns>
-        public static IColorable BoxToIColorable(Point point) => throw new NotImplementedException();
-
-        /// <summary>
         /// Performs an boxing conversion build-in <see cref="int"/> struct to <see cref="object"/> class.
         /// </summary>
         /// <param name="value"><see cref="int"/> object.</param>
         /// <returns><see cref="object"/> object.</returns>
         public static object BoxToObject(int value) => throw new NotImplementedException();
-
-        /// <summary>
-        /// Performs an boxing conversion build-in <see cref="int"/> struct to <see cref="ValueType"/> class.
-        /// </summary>
-        /// <param name="value"><see cref="int"/> object.</param>
-        /// <returns><see cref="ValueType"/> object.</returns>
-        public static ValueType BoxToValueType(int value) => throw new NotImplementedException();
-
-        /// <summary>
-        /// Performs an boxing conversion build-in <see cref="int"/> struct to <see cref="IFormattable"/> interface.
-        /// </summary>
-        /// <param name="value"><see cref="int"/> object.</param>
-        /// <returns><see cref="IFormattable"/> object.</returns>
-        public static IFormattable BoxToIFormattable(int value) => throw new NotImplementedException();
 
         /// <summary>
         /// Performs an boxing conversion custom <see cref="Color"/> enum to <see cref="object"/> class.
@@ -55,11 +27,39 @@ namespace TypeConversions
         public static object BoxToObject(Color color) => throw new NotImplementedException();
 
         /// <summary>
+        /// Performs an boxing conversion custom <see cref="Point"/> struct to <see cref="ValueType"/> class.
+        /// </summary>
+        /// <param name="point"><see cref="Point"/> object.</param>
+        /// <returns><see cref="ValueType"/> object.</returns>
+        public static ValueType BoxToValueType(Point point) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Performs an boxing conversion build-in <see cref="int"/> struct to <see cref="ValueType"/> class.
+        /// </summary>
+        /// <param name="value"><see cref="int"/> object.</param>
+        /// <returns><see cref="ValueType"/> object.</returns>
+        public static ValueType BoxToValueType(int value) => throw new NotImplementedException();
+
+        /// <summary>
         /// Performs an boxing conversion custom <see cref="Color"/> enum to <see cref="ValueType"/> class.
         /// </summary>
         /// <param name="color"><see cref="int"/> object.</param>
         /// <returns><see cref="ValueType"/> object.</returns>
         public static ValueType BoxToValueType(Color color) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Performs an boxing conversion custom <see cref="Point"/> struct to <see cref="IColorable"/> interface.
+        /// </summary>
+        /// <param name="point"><see cref="Point"/> object.</param>
+        /// <returns><see cref="IColorable"/> object.</returns>
+        public static IColorable BoxToIColorable(Point point) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Performs an boxing conversion build-in <see cref="int"/> struct to <see cref="IFormattable"/> interface.
+        /// </summary>
+        /// <param name="value"><see cref="int"/> object.</param>
+        /// <returns><see cref="IFormattable"/> object.</returns>
+        public static IFormattable BoxToIFormattable(int value) => throw new NotImplementedException();
 
         /// <summary>
         /// Performs an boxing conversion custom <see cref="Color"/> enum to <see cref="Enum"/> class.
